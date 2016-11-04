4b16a24a3c6b97d76878dc7e00253225044b1ecc
Garrett <g.robberson89@gmail.com>
2016-11-04 17:01:50 -0500

    Push Test

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); 
+
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
