88b7aa18ab003e8d633958c820bcfe7ee1046d98
Garrett <g.robberson89@gmail.com>
2016-11-05 11:01:20 -0500

    TypeScript with grunt included in config

--- app.js
+++ app.js
@@ -1,50 +1,37 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var hike = require('./routes/hike');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser()); 
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-app.get('/hikes', hike.index);
-app.post('/add_hike', hike.add_hike); // Web Api Set Up
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+"use strict";
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const indexRoute = require("./routes/index");
+class Server {
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    static bootstrap() {
+        return new Server();
+    }
+    config() {
+        this.app.set("views", path.join(__dirname, "views"));
+        this.app.set("view engine", "jade");
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(express.static(path.join(__dirname, "public")));
+        this.app.use(express.static(path.join(__dirname, "bower_components")));
+        this.app.use(function (err, req, res, next) {
+            var error = new Error("Not Found");
+            err.status = 404;
+            next(err);
+        });
+    }
+    routes() {
+        let router;
+        router = express.Router();
+        var index = new indexRoute.Index();
+        router.get("/", index.index.bind(index.index));
+        this.app.use(router);
+    }
+}
+var server = Server.bootstrap();
+module.exports = server.app;
